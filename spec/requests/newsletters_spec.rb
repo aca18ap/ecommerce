1c224009f97afb819819412ad5ed1195ae6398a0
William Thorne <wthorne1@sheffield.ac.uk>
2021-12-08 04:10:19 +0000

    Added some basic security tests for newsletters. Needs to be sorted when I am less tired

--- spec/requests/newsletters_spec.rb
+++ spec/requests/newsletters_spec.rb
@@ -29,5 +29,38 @@ describe 'Managing newsletters/registrations of interest' do
         expect(page).to have_content 'already registered'
       end
     end
+  end
+
+  context 'Security', js: true do
+    specify 'I cannot perform an SQL injection attack' do
+      skip 'COME BACK AND FINISH THIS ONE'
+      visit '/newsletters/new'
+      fill_in 'newsletter[email]', with: "'); DROP TABLE Newsletters--"
+      click_button 'Register Interest'
+      expect(page).to have_content "'); DROP TABLE Newsletters--"
+    end
+
+    specify 'I cannot give myself admin privileges via mass assignment', js: true do
+      visit '/'
+      click_link 'Edit my details'
+      page.execute_script "$('#new_newsletter').append(\"<input value='t' name='user[admin]'>\")"
+      sleep 1
+      click_button 'Update User'
+      expect(user2.reload.admin).to be false
+    end
+
+    specify 'I cannot perform an XSS attack' do
+      visit '/newsletters/new'
+      fill_in 'newsletter[email]', with: "<h1>Hello</h1>
+                                <script>
+                                  $(function() {
+                                    window.location.replace('http://api.rubyonrails.org/classes/ActionView/Helpers/SanitizeHelper.html');
+                                  });
+                                </script>"
+      click_button 'Register Interest'
+      sleep(2)
+      expect(current_url).not_to eq 'http://api.rubyonrails.org/classes/ActionView/Helpers/SanitizeHelper.html'
+    end
+  end
 end
 
