dfd4f12181e3102a92680cecb3aea1e8ce5a82df
Alberto <apia1@sheffield.ac.uk>
2021-12-09 05:50:13 +0000

    animations and general restyling

--- app/javascript/packs/features.js
+++ app/javascript/packs/features.js
@@ -1,12 +1,8 @@
 
-document.addEventListener('DOMContentLoaded', () => {
-    var features_list = document.getElementById('features-list').children;
-    var features = [];
-    for (let i=0; i< features_list.length; i++){
-        setTimeout(()=>{
-            features_list[i].visibility = "visible";
-            console.log(features_list[i]);
-        }, 1000)
-    }
-        
+$(document).ready(function() {
+    $('li#feature').each(function(i){
+        $(this).css({
+            "animation-delay": i + "s"
+        })
+    })
 })
