3a2f0d61f27b613ba7185f31761b6dfe9bfcf498
William Thorne <wthorne1@sheffield.ac.uk>
2021-12-10 12:02:34 +0000

    Commiting to pull

--- spec/models/faq_spec.rb
+++ spec/models/faq_spec.rb
@@ -15,29 +15,15 @@ require 'rails_helper'
 
 RSpec.describe Faq, type: :model do
   it 'is valid with valid attributes' do
-    faq = Faq.new(answer: 'a', question: 'b', hidden: false, usefulness: 0, created_at: '2021-11-27 16:39:22', updated_at: '2021-11-27 16:39:22')
+    faq = Faq.new(answer: 'a', question: 'b', hidden: false, usefulness: 0)
     expect(faq).to be_valid
   end
 
   it 'is not valid without a question' do
-    faq = Faq.new(answer: 'a', question: 'b', hidden: false, usefulness: 0, created_at: '2021-11-27 16:39:22', updated_at: '2021-11-27 16:39:22')
+    faq = Faq.new(answer: 'a', question: nil, hidden: false, usefulness: 0)
     expect(faq).not_to be_valid
   end
 
-  it 'is not valid without created_at' do
-    faq = Faq.new(answer: 'a', question: 'b', hidden: false, usefulness: 0, created_at: '2021-11-27 16:39:22', updated_at: '2021-11-27 16:39:22')
-    expect(faq).not_to be_valid
-  end
-
-  it 'is not valid without updated_at' do
-    faq = Faq.new(answer: 'a', question: 'b', hidden: false, usefulness: 0, created_at: '2021-11-27 16:39:22', updated_at: '2021-11-27 16:39:22')
-    expect(faq).not_to be_valid
-  end
-
-  it 'is not valid without created_at' do
-    faq = Faq.new(answer: 'a', question: 'b', hidden: false, usefulness: 0, created_at: '2021-11-27 16:39:22', updated_at: '2021-11-27 16:39:22')
-    expect(faq).not_to be_valid
-  end
 
 
 end
