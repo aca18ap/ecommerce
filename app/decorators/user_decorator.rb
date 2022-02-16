06e641d02b1be27a62e686eda7be18ca415455e2
William Thorne <wthorne1@sheffield.ac.uk>
2022-02-16 18:41:32 +0000

    Added truncate text decorator for reviews and updated tests to make use of decorator

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,23 +1,11 @@
 class UserDecorator < Draper::Decorator
   delegate_all
 
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  #   def created_at
-  #     helpers.content_tag :span, class: 'time' do
-  #       object.created_at.strftime("%a %m/%d/%y")
-  #     end
-  #   end
-
   def unlock_button?
-    if unlock_token.nil?
-      ''
-    else
-      h.link_to 'Unlock', h.unlock_user_path(id),
-                method: :patch, class: 'btn btn-xs',
-                data: { confirm: 'Are you sure you want to unlock this account?' }
-    end
-  end
+    return if unlock_token.nil?
 
+    h.link_to 'Unlock', h.unlock_user_path(id),
+              method: :patch, class: 'btn btn-xs',
+              data: { confirm: 'Are you sure you want to unlock this account?' }
+  end
 end
--- spec/decorators/review_decorator_spec.rb
+++ spec/decorators/review_decorator_spec.rb
@@ -1,4 +1,23 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe ReviewDecorator do
+  let(:review) { FactoryBot.create(:review).decorate }
+
+  describe '.truncated_description' do
+    context 'If a review description is under 120 characters long' do
+      it 'returns the full review description' do
+        review.description = 'Under 120 characters'
+        expect(review.truncated_description).to eq review.description
+      end
+    end
+
+    context 'If a review description is over 120 characters long' do
+      it 'returns 117 characters with an ellipse if the review description is more than 120 characters long' do
+        review.description = 'a' * 121
+        expect(review.truncated_description).to eq "#{'a' * 117}..."
+      end
+    end
+  end
 end
