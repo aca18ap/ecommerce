9de8a11e609585b5b6d6bf68dfbff6079383e9ee
Chris Berisford <cjberisford@live.co.uk>
2022-02-19 22:28:54 +0000

    SASS + Customer sign up

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,6 +12,8 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+require 'devise'
+
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
