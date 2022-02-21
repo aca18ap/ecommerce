287c3f5a1bdea20e59e539b6c5f721d4124644b8
Liam <liamthorne4@gmail.com>
2022-02-21 14:15:26 +0000

    Completed some more requests tests

--- spec/requests/metrics_spec.rb
+++ spec/requests/metrics_spec.rb
@@ -5,40 +5,37 @@ require 'rails_helper'
 RSpec.describe '/metrics', type: :request do
   before { login_as(FactoryBot.create(:admin)) }
 
-  describe 'GET /metrics' do
-    it 'should show graphs pertaining to registrations, feature views/shares and visits' do
-      skip 'WORK OUT HOW BEST TO TEST THIS FEATURE'
-    end
-  end
-
   describe 'POST /metrics' do
-    it 'should create a new visit from non-staff only pages' do
-      expect(Visit.count).to eq 0
+    context 'From non-staff-only pages' do
+      it 'should create a new visit' do
+        expect(Visit.count).to eq 0
 
-      post '/metrics', params: {
-        pageVisitedFrom: '1643892998084',
-        pageVisitedTo: '1643893033150',
-        path: '/',
-        csrf_token: ''
-      }
+        post '/metrics', params: {
+          pageVisitedFrom: '1643892998084',
+          pageVisitedTo: '1643893033150',
+          path: '/',
+          csrf_token: ''
+        }
 
-      expect(Visit.count).to eq 1
-      expect(Visit.last.path).to eq '/'
+        expect(Visit.count).to eq 1
+        expect(Visit.last.path).to eq '/'
+      end
     end
+    context 'From staff-only pages' do
+      it 'should not create a new visit' do
+        skip 'FEATURE NEEDS IMPLEMENTING'
+        expect(Visit.count).to eq 1
 
-    it 'should create a new visit from staff only pages' do
-      skip 'FEATURE NEEDS IMPLEMENTING'
-      expect(Visit.count).to eq(1)
+        post '/metrics', params: {
+          pageVisitedFrom: '1643892998084',
+          pageVisitedTo: '1643893033150',
+          path: '/admin/users',
+          csrf_token: ''
+        }
 
-      post '/metrics', params: {
-        pageVisitedFrom: '1643892998084',
-        pageVisitedTo: '1643893033150',
-        path: '/admin/users',
-        csrf_token: ''
-      }
-
-      expect(Visit.count).to eq 0
-      expect(Visit.last.path).to eq '/admin/users'
+        expect(Visit.count).to eq 0
+        expect(Visit.last.path).to eq '/admin/users'
+      end
     end
   end
 end
--- spec/requests/newsletters_spec.rb
+++ spec/requests/newsletters_spec.rb
@@ -1,1 +1,7 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
+
+RSpec.describe '/metrics', type: :request do
+
+end
