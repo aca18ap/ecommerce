80d6851171c3f003d560690244bc8e091f733adf
Alberto <apia1@sheffield.ac.uk>
2022-03-03 11:06:07 +0000

    Customized post text for each social

--- app/javascript/packs/sharing.js
+++ app/javascript/packs/sharing.js
@@ -1,49 +1,91 @@
 /**
- * Generate query text 
+ * Each feature's post description
  */
-function getQueryText(text){
-    let tmp = ""
-    ' '.split(text).forEach(e => {
-        tmp += e + '%20'
-    });
-    return tmp
+const posts = {
+    "Unlimited suggestions": "Get unlimited greener shopping suggestions, only on @ecommerce",
+    "One-click Visit retailer": "Easy access to green providers when shopping online on @ecommerce",
+    "24/7 Support": "Support available 24/7, only on @ecommerce",
+    "One-click access": "View the impact your shopping has on environment, always on @ecommerce",
+    "View purchase history": "Quick access to your carbon footprint, save the planet click by click on @ecommerce",
+    "Family purchase history": "Watch you and your family destroy the world at x4 the speed! On @ecommerce",
+    "View carbon footprint": "Quick access to your carbon footprint, save the planet click by click on @ecommerce",
+    "Carbon Footprint Viewer": "View an estimate of your cabon footprint and understand your impact on the environment, thanks to @ecommerce",
+    "Crowdsourced Data": "@ecommerce is powered by YOU! Help us populating our crowdsourced database and save the planet click by click",
+    "Browser Extension": "Easy access to green retailers when shopping thanks to @ecommerce 's browser extension"
 }
 
-
-fetch("posts.json")
-    .then(response.json())
-    .then(json => {
-        json.forEach(p => {
-            let posts = $('#'+p).children
-            
-        })
-    })
+const website_url = "team04.demo1.genesys.shefcompsciorg.uk"
 
 
-$(function(){
+/**
+ * Given a social and a feature, this function visits that social with prefilled text
+ * for that feature
+ * @param social
+ * @param feature
+ */
+function socialFormatter(social, feature) {
+    //Getting post text based on the feature
+    let post_text = posts[feature]
+    //Encoding post text in uri query form
+    let query_post = encodeURIComponent(post_text)
+    let url
+    //Each social requires a different uri format
+    if (social === "twitter") {
+        url = "https://twitter.com/intent/tweet?text=" + query_post
+    } else if (social === "email") {
+        url = "mailto:?subject=" + feature + "&body=" + query_post
+    } else if (social === "facebook") {
+        //Facebook doesn't allow text to be prefilled for a post, only links can be shared
+        //hence why the url is passed
+        url = "https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2F" + website_url + "%2F%23&src=sdkpreparse"
+    }
+    //The generated post link replaces the current website page (mainly to aid testing)
+    window.location.href = url
+
+}
+
+/**
+ * Listener for clicks on share icons. On click the feature and social are extracted
+ * through the DOM.
+ */
+$(function () {
     const CSRFToken = document.querySelector("meta[name='csrf-token']").getAttribute('content');
-    $('.share').each((idx, e)=>{
+    $('.share').each((idx, e) => {
         $(e).on(
             'click',
-            function(){
+            function () {
                 let mainCard = (e.parentNode.parentNode.parentNode.parentNode);
-                //let f = mainCard.getElementsByClassName("featureName")[0].textContent.replace(/(\r\n|\n|\r)/gm," ").trim()
-                sendData(e.id, mainCard.getElementsByClassName("featureName")[0].id);
+                let feature = mainCard.getElementsByClassName("featureName")[0].textContent.replace(/(\r\n|\n|\r)/gm, " ").trim()
+                let social = e.id
+
+                //sending data to record shares statistics
+                sendData(social, feature)
+
+                //Opening social page with prefilled feature text
+                socialFormatter(social, feature)
             }
         )
     })
 })
 
-function sendData(social, feature){
+
+/**
+ * Given a social and a feature, this function sends this data to the rails server
+ * so that statistics on shares can be recorded.
+ * @param social
+ * @param feature
+ */
+function sendData(social, feature) {
+
     const share = {
         social,
         feature,
     };
     const headers = {
-    type: 'application/json',
+        type: 'application/json',
     };
 
     const blob = new Blob([JSON.stringify(share)], headers);
+    navigator.sendBeacon('/shares', blob);
+}
 
-    navigator.sendBeacon('/shares', blob );
-} 
