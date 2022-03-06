61af81e5777a205d333d9b8471667a33ebbdf9be
Alberto <apia1@sheffield.ac.uk>
2022-03-06 22:26:08 +0000

    creation of materials and products materials table

---
+++ app/models/material.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: materials
+#
+#  id         :bigint           not null, primary key
+#  co2_per_kg :float
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Material < ApplicationRecord
+  has_many :products_material
+  has_many :products, through: :products_material
+
+end
