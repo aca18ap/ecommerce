67ea5af25cc9f164aec4b2d7a1f30e505c5754fc
Liam <liamthorne4@gmail.com>
2022-03-09 17:41:14 +0000

    Fixed final rubocop problems

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Helper for the application controller
 module ApplicationHelper
   def additional_javascript(name)
     content_for :additional_javascript, "#{content_for?(:additional_javascript) ? ' ' : ''}#{name}"
