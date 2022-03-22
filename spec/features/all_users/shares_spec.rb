159a0be18058c4a3b7164ea2a8ef7fb892bf3d92
Alberto <apia1@sheffield.ac.uk>
2022-03-22 23:39:02 +0000

    removed comments from test file

--- spec/features/all_users/shares_spec.rb
+++ spec/features/all_users/shares_spec.rb
@@ -5,19 +5,16 @@ describe 'Shares' do
   context 'When I visit the homepage' do
     context 'I can share the carbon footprint viewer' do
       specify 'by email', js: true do
-        # skip 'Feature needs fixing'
         visit '/'
         within(:css, '.carbon-footprint-feature') { find('#email').click }
       end
 
       specify 'to twitter', js: true do
-        # skip 'Feature needs fixing'
         visit '/'
         within(:css, '.carbon-footprint-feature') { find('#twitter').click }
       end
 
       specify 'to facebook', js: true do
-        # skip 'Feature needs fixing'
         visit '/'
         within(:css, '.carbon-footprint-feature') { find('#facebook').click }
       end
