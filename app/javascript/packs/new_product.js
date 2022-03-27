33cc8bcfed0141ef901f57434c96e0f5ccca5023
Alberto <apia1@sheffield.ac.uk>
2022-03-28 00:53:39 +0100

    front end validation + user

--- app/javascript/packs/new_product.js
+++ app/javascript/packs/new_product.js
@@ -1,11 +1,38 @@
 console.log('doc changed')
+
+
 setupListeners()
 
 
-function setupListeners(){
-    $('.percentage').on('input', function(){
+$(document).ready(function(){
+    $('.materials-section').on('click', function(e){
+        setupListeners()
+        checkPercentagesSum()
+        console.log('Setup listeners again')
+    })
+    $('.materials-section').on('cocoon:after-insert', (e) => {
+        setupListeners()
         checkPercentagesSum()
     })
+    $('.materials-section').on('cocoon:after-remove', (e) => {
+        setupListeners()
+        checkPercentagesSum()
+    })   
+    $('.materials-section').on('cocoon:before-insert', (e) => {
+        setupListeners()
+        checkPercentagesSum()
+    })
+    $('.materials-section').on('cocoon:before-remove', (e) => {
+        setupListeners()
+        checkPercentagesSum()
+    })
+})
+
+
+function setupListeners(){
+    $('.product_material_fields:hidden').off('input', checkPercentagesSum())
+    $('.product_material_fields:visible').on('input', checkPercentagesSum())
+   
 }
 
 $(function() {
@@ -17,14 +44,19 @@ function checkPercentagesSum(){
     let tmp = 100
     console.log("Check percentage called")
     Array.from(percentages).forEach(function(e){
-        console.log(parseInt(e.value))
-        tmp -= parseInt(e.value) || 0
+        console.log(e.offsetParent)
+        if (e.offsetParent !== null){
+            console.log(parseInt(e.value))
+            tmp -= parseInt(e.value) || 0
+
+        }
     })
     $('#sum').text(tmp)
     console.log(tmp)
 }
 
-$('.materials-section').on('click', function(e){
+/* $('.materials-section').on('click', function(e){
     setupListeners()
+    checkPercentagesSum()
     console.log('Setup listeners again')
-})
+}) */
