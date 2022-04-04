37084c13afce730b7b32105349255b14a5699268
Alberto <apia1@sheffield.ac.uk>
2022-04-04 17:13:23 +0100

    Can now toggle co2 breakdown

--- app/javascript/packs/show_product.js
+++ app/javascript/packs/show_product.js
@@ -1,5 +1,32 @@
 import { Loader } from "@googlemaps/js-api-loader";
 
+$(function(){
+  $('#chevron').on('click', function(){
+    toggleDetails()
+  })
+})
+
+function toggleDetails(){
+  let left = $('#co2_left')
+  let right = $('#co2_right')
+  let chevron = $('#chevron-icon')
+  if (chevron.hasClass('fa-chevron-left')){
+    console.log('reducing')
+    left.removeClass('col-xl-12')
+    left.addClass('col-xl-8')
+    right.css('display', 'block')
+    chevron.removeClass('fa-chevron-left')
+    chevron.addClass('fa-chevron-right')
+  }else{
+    console.log('expanding')
+    left.removeClass('col-xl-8')
+    left.addClass('col-xl-12')
+    right.css('display', 'none')
+    chevron.removeClass('fa-chevron-right')
+    chevron.addClass('fa-chevron-left')
+  }
+}
+
 // Create the script tag, set the appropriate attributes
 
 const loader = new Loader({
@@ -15,6 +42,12 @@ loader.load().then(()=>{
       zoom: 4,
       mapTypeId: 'terrain'
     });
+
+    /**
+     *  Not my code
+     *  https://stackoverflow.com/questions/20321006/curved-line-between-two-near-points-in-google-maps
+     *  */
+
     var p1 = new google.maps.LatLng(gon.lat1, gon.long1);
     var p2 = new google.maps.LatLng(gon.lat2, gon.long2);
 
@@ -61,54 +94,12 @@ loader.load().then(()=>{
           curveMarker2.setIcon(icon);
       });
   });
+  function zoomOut(){
+    map.setZoom(2)
+  }
+  
+  setTimeout(zoomOut, 1000)
+
 })
 
 
-
-/*Not my code */
-var p1 = new google.maps.LatLng(23.634501, -102.552783);
-var p2 = new google.maps.LatLng(17.987557, -92.929147);
-
-var markerP1 = new google.maps.Marker({
-    position: p1,
-    map: map
-});
-var markerP2 = new google.maps.Marker({
-    position: p2,
-    map: map
-});
-google.maps.event.addListener(map, 'projection_changed', function () {
-    var p1 = map.getProjection().fromLatLngToPoint(markerP1.getPosition());
-    var p2 = map.getProjection().fromLatLngToPoint(markerP2.getPosition());
-    var e = new google.maps.Point(p1.x - p2.x, p1.y - p2.y);
-    var m = new google.maps.Point(e.x / 2, e.y / 2);
-    var o = new google.maps.Point(0, 7);
-    var c = new google.maps.Point(m.x + o.x, m.y + o.y);
-    var curveMarker2 = new google.maps.Marker({
-        position: markerP1.getPosition(),
-        icon: {
-            path: "M 0 0 q " + c.x + " " + c.y + " " + e.x + " " + e.y,
-            scale: 24,
-            strokeWeight: 2,
-            fillColor: '#009933',
-            fillOpacity: 0,
-            rotation: 180,
-            anchor: new google.maps.Point(0, 0)
-        }
-    });
-    curveMarker2.setMap(map);
-    google.maps.event.addListener(map, 'zoom_changed', function () {
-        var zoom = map.getZoom();
-        var scale = 1 / (Math.pow(2, -zoom));
-        var icon = {
-            path: "M 0 0 q " + c.x + " " + c.y + " " + e.x + " " + e.y,
-            scale: scale,
-            strokeWeight: 2,
-            fillColor: '#009933',
-            fillOpacity: 0,
-            rotation: 180,
-            anchor: new google.maps.Point(0, 0)
-        };
-        curveMarker2.setIcon(icon);
-    });
-});
