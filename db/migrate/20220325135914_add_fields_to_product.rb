8ef7f603b9a3c5957f252ed989c5dd0f4c869e35
Liam <liamthorne4@gmail.com>
2022-04-07 17:01:32 +0100

    Trying to fix stack too deep errorr

--- db/migrate/20220325135914_add_fields_to_product.rb
+++ db/migrate/20220325135914_add_fields_to_product.rb
@@ -1,5 +1,5 @@
 class AddFieldsToProduct < ActiveRecord::Migration[6.1]
   def change
-    add_column :products, :price, :float, null: false
+    add_column :products, :price, :float, null: false, default: 0
   end
 end
