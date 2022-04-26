2c9f7a67914319af0ff5c4596e6b3d33b9c792d3
Alberto <apia1@sheffield.ac.uk>
2022-04-26 15:05:36 +0100

    Model tests for categories

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -17,5 +17,29 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:category) { FactoryBot.create(:category) }
+  describe 'Validates' do
+    it 'is valid with valid attributes' do
+      expect(category).to be_valid
+    end
+
+    it 'is invalid without name' do
+      category.name = ''
+      expect(category).not_to be_valid
+    end
+
+    it 'is valid without sub-categories' do
+      expect(category.children.length).to eq 0
+    end
+  end
+
+  describe 'Category hierarchy' do
+    let(:sub_category) { category.children.create(name: 'Sub-Category') }
+    it 'a sub-category belongs to a category' do
+      expect(sub_category.parent).not_to be_nil
+    end
+    it 'a category can have children' do
+      expect(category.children).not_to be_nil
+    end
+  end
 end
