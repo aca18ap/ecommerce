309492cfb3d17f2abc627c6194091fe9120df352
Alberto <apia1@sheffield.ac.uk>
2022-05-09 20:16:36 +0100

    Accessibility

---
+++ spec/integration/light/home_accessibility_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'Homepage accessibility' do
+    feature 'Home page', js: true do
+        scenario 'is accessible' do
+            visit '/'
+            expect(page).to be_axe_clean
+        end
+    end
+end
+
