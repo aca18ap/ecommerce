68bb8e2e3a08d9e9fc949a771dd3aa6140034551
Alberto <apia1@sheffield.ac.uk>
2022-05-10 00:03:48 +0100

    Fixed product cards in dark-mode

--- app/decorators/product_decorator.rb
+++ app/decorators/product_decorator.rb
@@ -125,11 +125,11 @@ class ProductDecorator < ApplicationDecorator
   end
 
   def expand_description
-    if description.size > 160
-      html_values = ''
-      html_values += '<a class="button collapsed" data-toggle=collapse href=#descriptionText '
-      html_values += 'aria-expanded=false aria-controls=descrpitionText></a>'
-      html_values.html_safe
-    end
+    return unless description.size > 160
+
+    html_values = ''
+    html_values += '<a class="button collapsed" data-toggle=collapse href=#descriptionText '
+    html_values += 'aria-expanded=false aria-controls=descrpitionText></a>'
+    html_values.html_safe
   end
 end
