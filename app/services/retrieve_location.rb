8b38822aa1ace5e9b7481e946541485c0175b85f
Liam <liamthorne4@gmail.com>
2022-05-12 20:51:03 +0100

    Added API key and linked to API for geocoder

--- app/services/retrieve_location.rb
+++ app/services/retrieve_location.rb
@@ -18,6 +18,8 @@ class RetrieveLocation
     else
       # IP = '90.204.36.252' if localhost to test on dev server
       geocode = Geocoder.search(@ip == '127.0.0.1' ? '94.3.89.188' : @ip).first
+
+      puts geocode.coordinates
       begin
         latitude = geocode.latitude
         longitude = geocode.longitude
