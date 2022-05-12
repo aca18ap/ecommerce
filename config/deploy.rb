9a237af39c167ed4465b3dd543bb3602d4f3afc7
Liam <liamthorne4@gmail.com>
2022-05-12 20:57:59 +0100

    Updated deploy script to include master key

--- config/deploy.rb
+++ config/deploy.rb
@@ -90,6 +90,16 @@ Rake::Task['deploy:assets:precompile'].clear_actions
 class PrecompileRequired < StandardError; end
 
 namespace :deploy do
+  namespace :check do
+    before :linked_files, :set_master_key do
+      on roles(:app), in: :sequence, wait: 10 do
+        unless test("[ -f #{shared_path}/config/master.key ]")
+          upload! 'config/master.key', "#{shared_path}/config/master.key"
+        end
+      end
+    end
+  end
+  
   namespace :assets do
     task :precompile do
       on roles(:app) do
